d1bd2e0d5af999b45b4bb93e327652ba8dc5be36
Theodora Tosheva <theodora.tosheva@sap.com>
2026-01-08 17:16:31 +0200

    feat: Add API reference documentation

--- pkg/apis/config/v1alpha1/doc.go
+++ pkg/apis/config/v1alpha1/doc.go
@@ -6,4 +6,8 @@
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
+//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/config.json -template-dir $GARDENER_HACK_DIR/api-reference/template -out-file ../../../../docs/api-reference/config.md
+
+// Package v1alpha1 contains the shoot trust confogurator configuration.
+// +groupName=config.trust-configurator.gardener.cloud
 package v1alpha1 // import "github.com/gardener/garden-shoot-trust-configurator/pkg/apis/config/v1alpha1"
