4a3d32ccec5dc0c3c8028fce3c98220092cace34
Theodora Tosheva <theodora.tosheva@sap.com>
2026-01-29 14:28:20 +0200

    chore: update Makefile and scripts for code generation (#76)

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -8,16 +8,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# setup virtual GOPATH
-source "$GARDENER_HACK_DIR"/vgopath-setup.sh
-
 CODE_GEN_DIR=$(go list -m -f '{{.Dir}}' k8s.io/code-generator)
 source "${CODE_GEN_DIR}/kube_codegen.sh"
 
-rm -f $GOPATH/bin/*-gen
-
-PROJECT_ROOT=$(dirname $0)/..
-
 kube::codegen::gen_helpers \
   --boilerplate "${GARDENER_HACK_DIR}/LICENSE_BOILERPLATE.txt" \
-  "${PROJECT_ROOT}/pkg/apis/config"
+  "${REPO_ROOT}/pkg/apis/config"
